60831c1ef34dd59717dbef059f0690bdbaf00758
Damian Brojek <damianbrojek@gmail.com>
2023-06-29 11:30:04 +0200

    update voor mad

--- MAD/mad/lib/main.dart
+++ MAD/mad/lib/main.dart
@@ -1,17 +1,26 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
 
   final List<Widget> _tabs = [
@@ -22,44 +31,76 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('SummaMove'),
-        ),
-        body: _tabs[_currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (int index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.directions_run),
-              label: 'Excercises',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.scoreboard),
-              label: 'Score',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Tab 3',
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SummaMove'),
+      ),
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_run),
+            label: 'Exercises',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.scoreboard),
+            label: 'Score',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Tab 3',
+          ),
+        ],
       ),
     );
   }
 }
 
-class Tab1 extends StatelessWidget {
+class Tab1 extends StatefulWidget {
+  @override
+  _Tab1State createState() => _Tab1State();
+}
+
+class _Tab1State extends State<Tab1> {
+  List<dynamic> exercises = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchExercises();
+  }
+
+  Future<void> fetchExercises() async {
+    final response = await http.get(Uri.parse('http://127.0.0.1:8000/api/exercises'));
+    if (response.statusCode == 200) {
+      setState(() {
+        exercises = jsonDecode(response.body);
+      });
+    } else {
+      // Handle error response
+      print('Failed to fetch exercises');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Excercises'),
+    return exercises.isEmpty
+        ? Center(child: CircularProgressIndicator())
+        : ListView.builder(
+      itemCount: exercises.length,
+      itemBuilder: (BuildContext context, int index) {
+        final exercise = exercises[index];
+        return ListTile(
+          title: Text(exercise['name']),
+          subtitle: Text(exercise['description']),
+        );
+      },
     );
   }
 }
